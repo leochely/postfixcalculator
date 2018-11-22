427bf6e75e322c4134b8d43acb9c0cbf56abfcad
Léo Chely <31514354+leochely@users.noreply.github.com>
2018-11-22 15:44:17 -0700

    Getting error on thtype error on  'pop', trying to fix it

--- postfix.ml
+++ postfix.ml
@@ -14,3 +14,14 @@ let print_list f lst =
   print_string "]";;
 
 print_list print_string p;;
+
+let compute lst =
+  let s = Stack.create in
+    let rec process_elements = function
+      | [] -> Stack.pop () s
+      | "x"::t -> let temp = Stack.pop s () + Stack.pop s () in Stack.push temp s; process_elements t
+      | "-"::t -> let temp = Stack.pop s () - Stack.pop s () in Stack.push temp s; process_elements t
+      | "x"::t -> let temp = Stack.pop s () * Stack.pop s () in Stack.push temp s; process_elements t
+      | "/"::t -> let temp = Stack.pop s / Stack.pop s in Stack.push temp s; process_elements t
+      | x::t -> Stack.push a s
+in compute p;;
