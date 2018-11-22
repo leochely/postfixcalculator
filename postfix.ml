8ff7f44dd8be10b2b14a56db69098915dd4987e7
SebastianCabrol <sebastian.cabrol@gmail.com>
2018-11-22 11:30:22 -0700

    used different string splitting technique, p is currently list of strings

--- postfix.ml
+++ postfix.ml
@@ -1,17 +1,16 @@
-use "topfind"
+(*use "topfind"
 require "str"
-
+*)
 let expression = "2 1 +";;
-
-let s = (Str.split (Str.regexp " ") expression);;
+let p = (String.split_on_char ' ' expression)
 
 let print_list f lst =
   let rec print_elements = function
     | [] -> ()
-    | h::t -> f h; print_string ";"; print_elements t
+    | h::t -> f h; print_string ", "; print_elements t
   in
   print_string "[";
   print_elements lst;
   print_string "]";;
 
-print_list print_string s;;
+print_list print_string p;;
