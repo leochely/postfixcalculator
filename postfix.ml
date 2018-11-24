308e4dba546461e49110be8dec2f78c93ae4ca6e
Léo Chely <31514354+leochely@users.noreply.github.com>
2018-11-23 20:27:43 -0700

    Syntax error fixed, goodgood result on the basic input

--- postfix.ml
+++ postfix.ml
@@ -19,10 +19,12 @@ let compute lst =
   let s = Stack.create () in
     Stack.push 0 s;
     let rec process_elements = function
-      | [] -> Stack.pop s
+      | [] -> let result = Stack.pop s in print_int result
       | "+"::t -> let temp = Stack.pop s + Stack.pop s in Stack.push temp s; process_elements t
       | "-"::t -> let temp = Stack.pop s - Stack.pop s in Stack.push temp s; process_elements t
       | "x"::t -> let temp = Stack.pop s * Stack.pop s in Stack.push temp s; process_elements t
       | "/"::t -> let temp = Stack.pop s / Stack.pop s in Stack.push temp s; process_elements t
       | x::t -> Stack.push (int_of_string x) s; process_elements t
-in compute p;;
+    in process_elements lst;;
+
+compute p;;
