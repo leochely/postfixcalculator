99b9401ace239b77da4ce2270301c6ee04a53bb6
SebastianCabrol <sebastian.cabrol@gmail.com>
2018-11-26 09:10:29 -0700

    Code finished except for error on while loop

--- postfix.ml
+++ postfix.ml
@@ -1,9 +1,3 @@
-(*use "topfind"
-require "str"
-*)
-let expression = "2 1 +";;
-let p = (String.split_on_char ' ' expression)
-
 let print_list f lst =
   let rec print_elements = function
     | [] -> ()
@@ -12,8 +6,6 @@ let print_list f lst =
   print_string "[";
   print_elements lst;
   print_string "]";;
-
-(*List.map (print_string) p;;*)
 
 let compute lst =
   let s = Stack.create () in
@@ -27,13 +19,17 @@ let compute lst =
       | x::t -> Stack.push (int_of_string x) s; process_elements t
     in process_elements lst;;
 
-compute p;;
-print_string "\n";;
+print_string "Welcome to the OCaml PostFix Calculator\n";;
+print_string "-----------------------------------------\n";;
 
-let in_channel = open_in;;
+let quit = false;;
 
-while true do
-    let expression = (input_line in_channel);
-    compute (String.split_on_char ' ' expression);
-    print_string "\n"
+while quit != true do
+    print_string "Input: ";
+    let expression = read_line ();
+    if expression == "quit" || exression == "q" then 
+        quit = true
+    else
+        compute (String.split_on_char ' ' expression);
+    print_string "\n";
 done;;
